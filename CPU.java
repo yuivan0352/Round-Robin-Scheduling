904a0b2c3c10a4216fbc80821f9c54767f294508
NarwhalTaco <yu.ivan0352@gmail.com>
2023-04-01 16:17:52 -0400

    Updated to be able to simulate scheduling

--- CPU.java
+++ CPU.java
@@ -1,4 +1,44 @@
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class CPU {
-    public CPU() {
+    private static LinkedList<Process> queue = new LinkedList<Process>();
+    private static int clock = 0;
+    private static int quantum, conSwitch;
+
+    public static void main(String args[]) throws FileNotFoundException{
+        try {
+            ProcessCreator pc = new ProcessCreator("processes.csv");
+            queue = pc.queueProcesses();
+            quantum = Integer.parseInt("2");
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found");
+            System.exit(0);
+        }
+
+        while (queue.size() != 0) {
+            queue.peek().decreaseBurst(quantum);
+            if (queue.peek().getRemBurst() < 0) {
+                clock += quantum + queue.peek().getRemBurst();
+                queue.peek().setRemBurst(0);
+            } else {
+                clock += quantum;
+            }
+            System.out.println(clock + ": " + queue.peek().getPid() + ", " + queue.peek().getRemBurst());
+            if (queue.peek().getRemBurst() <= 0) {
+                queue.peek().setCompletion(clock);
+                queue.peek().calcTurnaround();
+                queue.peek().calcWaiting();
+                queue.peek().calcResponse(clock);
+                System.out.println(queue.peek());
+                queue.pop();
+            } else if (queue.peek().getRemBurst() != 0) {
+                queue.offer(queue.pop());
+            }
+            conSwitch++;
+        }
+
+        System.out.println(clock);
+        System.out.println(conSwitch);
     }
 }
--- Process.java
+++ Process.java
@@ -1,16 +1,59 @@
+import java.io.FileReader;
+
 public class Process {
-    private int pid, arrive, burst;
+    private int pid, arrival, burst, remBurst, completion, turnaround, waiting, response;
     
-    public Process(int pid, int arrive, int burst) {
+    public Process(int pid, int arrival, int burst) {
         this.pid = pid;
-        this.arrive = arrive;
+        this.arrival = arrival;
         this.burst = burst;
+        this.remBurst = burst;
     }
 
-    public int getPid()                    {return pid;}
-    public void setPid(int newPid)         {pid = newPid;}
-    public int getArrive()                 {return arrive;}
-    public void setArrive(int newArrive)   {arrive = newArrive;}
-    public int getBurst()                  {return burst;}
-    public void setBurst(int newBurst)     {burst = newBurst;}
+    public void calcTurnaround() {
+        turnaround = completion - arrival;
+    }
+
+    public void calcWaiting() {
+        waiting = turnaround - burst;
+    }
+
+    public void calcResponse(int firstClock) {
+        response = firstClock - arrival;
+    }
+
+    public void decreaseBurst(int quantum) {
+        remBurst -= quantum;
+    }
+
+    public int getPid()                         {return pid;}
+    public void setPid(int newPid)              {pid = newPid;}
+    public int getArrival()                     {return arrival;}
+    public void setArrival(int newArrival)      {arrival = newArrival;}
+    public int getBurst()                       {return burst;}
+    public void setBurst(int newBurst)          {burst = newBurst;}
+    public int getRemBurst()                    {return remBurst;}
+    public void setRemBurst(int newRem)         {remBurst = newRem;}
+    public int getCompletion()                  {return completion;}
+    public void setCompletion(int newComplete)  {completion = newComplete;}
+    public int getTurnaround()                  {return turnaround;}
+    public void setTurnaround(int newTurnaround){turnaround = newTurnaround;}
+    public int getWaiting()                     {return waiting;}
+    public void setWaiting(int newWaiting)      {waiting = newWaiting;}
+    public int getResponse()                    {return response;}
+    public void setResponse(int newResponse)    {response = newResponse;}
+    
+
+    public String toString() {
+        String tmp = "";
+        tmp += "Process Info:\n";
+        tmp += "Process ID: " + pid + "\n";
+        tmp += "Arrival Time: " + arrival + "\n";
+        tmp += "Burst Time: " + burst + "\n";
+        tmp += "Completion Time: " + completion + "\n";
+        tmp += "Turnaround Time: " + turnaround + "\n";
+        tmp += "Waiting Time: " + waiting + "\n";
+        tmp += "Response Time: " + response + "\n";
+        return tmp;
+    }
 }
